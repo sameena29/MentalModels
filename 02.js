b01316489cc16eaf78f2aa5f273574b535309ba6
sameena29 <sameenashaik891@gmail.com>
2022-12-26 18:11:53 +0530

    chapter2

---
+++ 02.js
@@ -0,0 +1,23 @@
+//https://signalvnoise.com/posts/3124-give-it-five-minutes => it's easy to push back so give it five minutes
+
+//  Functions are objects, but because they include a few unique additional features, 
+//  we’re going to refer to them separately to avoid confusion.
+
+console.log(2);
+console.log("hello");
+console.log(undefined);
+//result
+2
+hello
+undefined
+
+console.log({});
+console.log([]);
+console.log(x => x * 2);
+//result
+{}
+[[Prototype]]: Object
+[]
+length: 0[[Prototype]]: Array(0)
+x => x * 2
+undefined
